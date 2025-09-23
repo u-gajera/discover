71345edcae17cf25416f107e26e2a3be6ffddb74
u-gajera <gajera.uday.1@gmail.com>
2025-09-23 16:41:26 +0200

    setup file to publish in the pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,47 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='pysisso',
-    version='0.1.0',
-    description='A Python Implementation of Sure Independence Screening and Sparsifying Operator',
-    author='Your Name',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discover-symbolic-regression",
+    version="0.1.0",
+    author="Uday Gajera",
+    description="Data-Informed Symbolic Combination " \
+                "of Operators for Variable Equation Regression.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    
+    # Classifiers help users find your project on PyPI
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Chemistry",
+    ],
+    
+    python_requires='>=3.8',
+    
+    # Core dependencies
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "sympy",
+        "joblib",
+        "matplotlib",
+    ],
+    
+    extras_require={
+        "plotting": ["seaborn"],
+        "units": ["pint"],
+        "miqp": ["gurobipy"],
+        # Note: The user must install the correct CuPy version for their CUDA toolkit.
+        # e.g., 'cupy-cuda11x' for CUDA 11.x or 'cupy-cuda12x' for CUDA 12.x.
+        "gpu": ["torch", "cupy"], 
+        "all": ["seaborn", "pint", "gurobipy", "torch", "cupy"],
+    }
 )
