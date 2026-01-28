20065e5aaee5ab9aedefd96046f7797b22455023
u-gajera <gajera.uday.1@gmail.com>
2026-01-28 11:11:05 +0100

    adding test dataset

--- tests/test_discover.py
+++ tests/test_discover.py
@@ -7,7 +7,9 @@ from discover.models import DiscoverRegressor
 @pytest.fixture
 def sample_data():
     """Loading the sample dataset."""
-    df = pd.read_csv('sample_dataset.csv')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(current_dir, 'sample_dataset.csv')
+    df = pd.read_csv(csv_path)
     X = df[['feature1', 'feature2', 'feature3']]
     y = df['property']
     return X, y
